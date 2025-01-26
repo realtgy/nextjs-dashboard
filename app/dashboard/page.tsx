cd0b14c4d1e56b838ad0094588a6c359c36e906c
you tang <tangbob171@gmail.com>
2025-01-26 14:59:22 +0800

    feat: lastest invoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,14 @@
-import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
- import { fetchRevenue } from '@/app/lib/data';
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchRevenue, fetchLatestInvoices } from "@/app/lib/data";
 
 export default async function Page() {
-  const [revenue] = await Promise.all([
-    fetchRevenue()
-  ])
+  const [revenue, latestInvoices] = await Promise.all([
+    fetchRevenue(),
+    fetchLatestInvoices(),
+  ]);
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -24,8 +25,8 @@ export default async function Page() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
