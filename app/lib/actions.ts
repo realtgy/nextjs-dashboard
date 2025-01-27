c23b810c8c4a86e1c7eaf16ecbdbbac817cd8613
you tang <tangbob171@gmail.com>
2025-01-27 09:50:45 +0800

    feat: delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -54,4 +54,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   }
 
 
-  
+  export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  }
